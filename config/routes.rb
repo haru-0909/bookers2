3ec1fad238c72591b76d1eb8a3ac83e8a4f64b96
EC2 Default User <hrk0911tkym@docomo.ne.jp>
2022-09-28 00:36:44 +0000

    [Add]ルーティングの記載とアソシエーションの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
   devise_for :users
   root to: 'homes#top'
+
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
