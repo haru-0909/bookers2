7366aa3db9431c9e2881bf7f522cb6e9dd9fbe87
EC2 Default User <hrk0911tkym@docomo.ne.jp>
2022-09-30 11:06:09 +0000

    [Add]レイアウト作成と編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: :about
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 
