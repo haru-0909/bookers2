96bde582bf1a6c499e1f73d997fffe78c08978e6
EC2 Default User <hrk0911tkym@docomo.ne.jp>
2022-10-01 17:18:38 +0000

    [Add]showの途中まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,22 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book new
-  end
+    def index
+        @book = Book.new
+        @books = Book.all
+    end
+    
+    def show
+      @book = Book.find(params[:id])
+
+     def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+     end
+
+      private
+
+    def book_params
+    params.require(:book).permit(:title, :body)
+    end
 end
