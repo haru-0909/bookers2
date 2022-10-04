f0ef8a118022ff3d0b0aa66de111a438a3d54fec
EC2 Default User <ec2-user@ip-172-31-15-191.ap-northeast-1.compute.internal>
2022-10-04 05:18:54 +0000

    [Add]userのindexページの途中まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,20 @@ class BooksController < ApplicationController
     def index
         @book = Book.new
         @books = Book.all
+        @user = current_user
     end
-    
+
     def show
       @book = Book.find(params[:id])
+      @user = User.find(@book.user_id)
+    end
 
-     def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
-     end
+    def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        @book.save
+        redirect_to book_path(@book.id)
+    end
 
       private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+    @user = current_user
+  end
+
+
   def show
     @user = Book.find(params[:id])
     @books = @user.books
